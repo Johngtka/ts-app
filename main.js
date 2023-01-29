bbfab2234711f6008009e5e0ed4d0c010464df7d
Johngtka <johngtka@gmail.com>
2023-01-29 12:27:34 +0100

    fix: hide function

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 var Dog = /** @class */ (function () {
     function Dog() {
         this.name = 'Zoe';
-        this.age = 10;
         this.haircolor = ['red', 'white'];
+        this.date = new Date();
+        this.time = this.ageCalck(10);
+        this.iteration = 0;
     }
-    Dog.prototype.time = function () {
-        var date = new Date();
-        var y = date.getFullYear();
-        console.log(y - this.age);
-        document.body.innerHTML = 'My dog name is ' + this.name + ' and she is ' + this.age + ' years old, her hair color is ' + this.haircolor;
+    Dog.prototype.ageCalck = function (x) {
+        var y = this.date.getFullYear();
+        console.log(y - x);
+        return x;
+    };
+    Dog.prototype.possIt = function () {
+        var par = document.createElement('p');
+        document.body.appendChild(par);
+        par.innerHTML = 'My dog name is ' + this.name + ' and she is ' + this.time + ' years old, her hair color is ' + '<span style="background-color:#ff9d58;">' + this.haircolor[0].toUpperCase() + '</span>' + ' and ' + '<span style="border:1px solid #000;">' + this.haircolor[1].toUpperCase() + '</span>';
+        par.style.textAlign = 'center';
+        par.classList.add('ts-txt');
+        // console.log(this.time)
+    };
+    Dog.prototype.render = function () {
+        for (var i = this.iteration; i <= this.time; i++) {
+            console.log(i);
+        }
     };
     return Dog;
 }());
 var c = new Dog();
-c.time();
+c.time;
+c.possIt();
+c.render();
--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
 class Dog{
     name = 'Zoe'
-    age = 10
-    haircolor=['red','white']
-    time(){
-        const date = new Date()
-        let y = date.getFullYear()
-        console.log(y-this.age)
-        document.body.innerHTML = 'My dog name is '+this.name+' and she is '+this.age+' years old, her hair color is '+this.haircolor
+    haircolor = ['red','white']
+    date = new Date()
+    ageCalck(x){
+        let y = this.date.getFullYear()
+        console.log(y-x)
+        return x
     }
+    time = this.ageCalck(10)
+    possIt(){
+        const par = document.createElement('p')
+        document.body.appendChild(par)
+        par.innerHTML= 'My dog name is '+this.name+' and she is '+this.time+' years old, her hair color is '+'<span style="background-color:#ff9d58;">'+this.haircolor[0].toUpperCase()+'</span>'+' and '+'<span style="border:1px solid #000;">'+this.haircolor[1].toUpperCase()+'</span>'
+        par.style.textAlign='center'
+        par.classList.add('ts-txt')
+        // console.log(this.time)
+    }
+    // iteration = 0
+    // render(){
+    //     for(let i = this.iteration;i<=this.time;i++){
+    //         console.log(i)
+    //     }
+    // }
 }
 const c = new Dog()
-c.time()
+c.time
+c.possIt()
+// c.render()
